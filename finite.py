bb1d9cdb0c6253689a675c9b92ce19cb5717e07a
HalcyonAQ <114362829+HalcyonAQ@users.noreply.github.com>
2022-10-16 06:30:15 -0500

    Update finite.py

--- finite.py
+++ finite.py
@@ -28,25 +28,34 @@ class DifferenceUniformGrid:
         self.convergence_order = convergence_order
         self.stencil_type = stencil_type
         self.grid = grid
-
+        
+        
+        ##define needed variables
         size = int(2*math.floor((self.derivative_order+1)/2)+self.convergence_order-1)
         
         j = np.arange(int(-(size+1)/2+1),int((size+1)/2))
         b = np.zeros(size,dtype = int)
         b[self.derivative_order] = 1
         S = []
+        
+        ##create s matrix
         i = 0
         while i<size:
             S.append(list(1/factorial(i)*(j*self.grid.dx)**i))
             i = i+1
             
+        ##compute a matrix from s matrix
         a = np.linalg.inv(S) @ b
+        
+        ##assign values to sparse matrix
         D = sparse.diags(a, offsets=j, shape = [int(self.grid.N),int(self.grid.N)])
         D = D.tocsr()
+        
+        ##impute corner cases
         p = 0
         q = 0
         r = 0
-        while p < N:
+        while p < size:
             while q < size:
                 D[p,j[q]] = a[r]
                 D[-(p+1),-j[q]-1] = a[-r-1]
@@ -73,12 +82,49 @@ class DifferenceNonUniformGrid:
         self.stencil_type = stencil_type
         self.grid = grid
         
+        
+        c = math.floor(self.convergence_order/2)
+        
+        j = np.arange(-c,c+1)
+        b = np.zeros(2*c+1,dtype = int)
+        b[self.derivative_order] = 1
+        S = []
+        i = 0
+        while i<2*c+1:
+            S.append(list(1/factorial(i)*(j*self.grid.dx)**i))
+            i = i+1
+        a = np.linalg.inv(S) @ b
+        D = sparse.diags(a, offsets=j, shape = [5,5])
+        D = D.tocsr()
+        p = 0
+        while p < len(j)/2:
+            D[0,j[p]] = a[p]
+            D[j[p],0] = a[-p-1]
+            p = p + 1
+        
+        
+        self.matrix = D
+
+    def __matmul__(self, other):
+        return self.matrix @ other
+    
+
+
+class DifferenceNonUniformGrid:
+
+    def __init__(self, derivative_order, convergence_order, grid, stencil_type='centered'):
+        self.derivative_order = derivative_order
+        self.convergence_order = convergence_order
+        self.stencil_type = stencil_type
+        self.grid = grid
+        
         ##define needed variables
-        size = int(2*math.floor((self.derivative_order+1)/2)+self.convergence_order-1)
+        size = convergence_order + derivative_order  + 2
         
         j = np.arange(int(-(size+1)/2+1),int((size+1)/2))
         b = np.zeros(size,dtype = int)
         b[self.derivative_order] = 1
+        
         
         grid = self.grid.values
         length = self.grid.length
@@ -130,23 +176,37 @@ class DifferenceNonUniformGrid:
                     u[v] = length-grid[-1]
                 v = v+1
 
+        jn = np.array([list(np.zeros(size)) for x in range(N)])
+        i = 0
+        m = 0
+        while i<N:
+            while m<(size-1)/2:
+                jn[i][m] = -sum(h[i][m:int((size-1)/2)])
+                m = m+1
+            h[i][m] = 0
+            m = m+1
+            while m<size:
+                jn[i][m] = sum(h[i][int((size-1)/2):m+1])
+                m = m+1
+            m = 0
+            i = i+1
 
-
-        ##create a matrix
+        
+        
+        ##create a matrix using individual s matrix
         a = [np.zeros(size).tolist() for x in range(N)]
-        print(list(a))
         n = 0
         while n < N:
             S = []
             i = 0
-            x_counter = 0
             while i<size:
-                S.append(list(1/factorial(i)*(j*h[n])**i))
+                S.append(list(((jn[n])**i)/factorial(i)))
                 i = i+1
-            a[n] = (np.linalg.inv(S) @ b).tolist()
+            a[n] = np.linalg.solve(S,b)
+           # print(a[n])
             n = n+1
 
-
+        
 
         ##transpose a
         counter = 0
@@ -154,19 +214,26 @@ class DifferenceNonUniformGrid:
         while counter <size:  
             ax.append([sub[counter] for sub in a])
             counter = counter + 1
-
-
-
+        
+        print(ax)
+        c = 0
+        while c<(size-1)/2:
+            if j[c]!=0:
+                ax[c] = ax[c][abs(j[c]):N-abs(j[c])+5] + [0 for x in range(abs(j[c]))]
+                c = c+1
+            else:
+                c = c+1
+        print(np.array(ax))
+        
         ##create sparse matrix
-        D = sparse.diags(ax, offsets=j, shape = [N,N])
+        D = sparse.diags(ax, offsets=j,shape = [N,N])
         D = D.tocsr()
 
         ##impute corner cases
         p = 0
         q = 0
         r = 0
-        
-        while p < N:
+        while p < size:
             while q < size:
                 D[p,j[q]] = a[p][r]
                 D[-(p+1),-j[q]-1] = a[-p-1][-r-1]
@@ -175,11 +242,58 @@ class DifferenceNonUniformGrid:
             p = p + 1
             q = p
             r = 0 
-        
+
         ##assign attribute
         self.matrix = D
 
+
     def __matmul__(self, other):
         return self.matrix @ other
+    
+    
+class CenteredFiniteDifference(Difference):
 
+    def __init__(self, grid):
+        h = grid.dx
+        N = grid.N
+        j = [-1, 0, 1]
+        diags = np.array([-1/(2*h), 0, 1/(2*h)])
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
+        matrix = matrix.tocsr()
+        matrix[-1, 0] = 1/(2*h)
+        matrix[0, -1] = -1/(2*h)
+        self.matrix = matrix
+
+
+class CenteredFiniteSecondDifference(Difference):
+
+    def __init__(self, grid):
+        h = grid.dx
+        N = grid.N
+        j = [-1, 0, 1]
+        diags = np.array([1/h**2, -2/h**2, 1/h**2])
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
+        matrix = matrix.tocsr()
+        matrix[-1, 0] = 1/h**2
+        matrix[0, -1] = 1/h**2
+        self.matrix = matrix
+
+
+class CenteredFiniteDifference4(Difference):
+
+    def __init__(self, grid):
+        h = grid.dx
+        N = grid.N
+        j = [-2, -1, 0, 1, 2]
+        diags = np.array([1, -8, 0, 8, -1])/(12*h)
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
+        matrix = matrix.tocsr()
+        matrix[-2, 0] = -1/(12*h)
+        matrix[-1, 0] = 8/(12*h)
+        matrix[-1, 1] = -1/(12*h)
+
+        matrix[0, -2] = 1/(12*h)
+        matrix[0, -1] = -8/(12*h)
+        matrix[1, -1] = 1/(12*h)
+        self.matrix = matrix
 
