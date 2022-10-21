5d212f173b4043927a78bb49f4518140ac560b9b
HalcyonAQ <114362829+HalcyonAQ@users.noreply.github.com>
2022-10-21 11:19:44 -0500

    Update timesteppers.py

--- timesteppers.py
+++ timesteppers.py
@@ -229,7 +229,7 @@ class BackwardDifferentiationFormula(Timestepper):
                 i = i+1
             a = np.zeros(k)
             b = np.zeros(k)
-            b[1] = self.dt[[-2]
+            b[1] = self.dt[-2]
             a = np.linalg.inv(s).dot(b)
             a0 = 1/a[0]
             a = a*a0
